2d2452eccf63748a2ce9832e3b2a9a5f9433ca7e
gayatrigurav93 <gayatrigurav93@gmail.com>
2026-07-24 13:02:18 +0530

    update index.html and portf.js mailbtn

--- portf.js
+++ portf.js
@@ -1,2 +1,5 @@
 
-
+const mailbtn = document.getElementById("mailbtn");
+mailbtn.addEventListener("click", ()=> {
+window.open("https://mail.google.com/mail/?view=cm&fs=1&to=gurav.gayatri.r@gmail.com" , "_blank");
+});
